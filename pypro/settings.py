4fbbb8c18ae3164b980bc27816a4f1eb446e4f37
Gustavo Suto <guyrux@gmail.com>
2021-03-08 00:02:15 -0300

    [fix] Removed an unused lib.

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from typing import cast
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
