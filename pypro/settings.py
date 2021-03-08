cfed961cf4aec961aa106259de0c32c1687571fb
Gustavo Suto <guyrux@gmail.com>
2021-03-08 00:00:44 -0300

    [feat] Created contrib and env file.

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from typing import cast
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ku4t2ixs472=!(nixml5k*p8n8%+=)*ju0lk^$8f2*hg16qiad'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
